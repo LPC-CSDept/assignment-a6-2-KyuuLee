4225695b40d17423c9f28834f1132b50e05e461d
Kyu Lee <kwooler@gmail.com>
2022-09-23 22:11:16 -0700

    1

--- A62.hpp
+++ A62.hpp
@@ -3,15 +3,24 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-
 double nthpowerp(int n)
 {
-        // return n_th power number
+        int i;
+        double pow = 1.0;
+        for (i = 0; i < n; i++)
+                pow *= 2;
+        return pow;
 }
 double nthpowern(int n)
 {
-        // return n_th power number
+        int i;
+        double pow = 1.0;
+        for (i = n; i < 0; i++)
+                pow *= 2;
+        pow = 1.0 / pow;
+        return pow;
 }
+
 void printout(double result)
 {
         cout << "The result is " << setw(10) << fixed << setprecision(5) << result << endl;
